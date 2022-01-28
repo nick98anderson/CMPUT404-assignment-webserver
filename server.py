d8bd0d4974c89179d319d1afbc087fb82a73f4ee
Nick Anderson <47461646+nick98anderson@users.noreply.github.com>
2022-01-28 09:31:21 -0700

    finished server

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
 #  coding: utf-8 
+from posixpath import abspath
 import socketserver
+from urllib import response
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -32,7 +38,144 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        #self.request.sendall(bytearray("OK",'utf-8'))
+
+        req = self.data.split()[0].decode()
+        uri = self.data.split()[1].decode()
+
+        if (req!= 'GET'):
+            self.method_not_allowed_405()
+
+        else:
+            self.response(uri)
+        
+
+
+    def response(self, uri):
+
+        path = self.append_path(uri)
+        mime_type = self.get_mime_type(uri)
+        directory = os.path.abspath('www')
+
+        if not path.startswith(directory):
+            self.not_found_404() 
+        elif not os.path.exists(path):
+            self.not_found_404()
+        elif os.path.isdir(path):
+            if uri[-1] == '/':
+                path = path + "/index.html"
+            else:
+                self.redirect_301(path)
+         
+        try:
+            file = open(path, "r")
+            content = file.read()
+            self.ok_200(content, mime_type)
+        except IOError:
+            self.not_found_404()
+
+
+        
+    def ok_200(self,content, mime_type):
+        date = self.get_time_formated()
+        http_status = "HTTP/1.1 200 OK" 
+        response = http_status + "\r\n" + \
+                "Server: " + "Nick's Server" + "\r\n" + \
+                "Date: " + str(date) + "\r\n" + \
+                "Content-type: text/" + mime_type + "; charset=UTF-8" + "\r\n" \
+                "Content-length: " + str(len(content)) + "\r\n\r\n" + \
+                content + "\r\n"
+        
+        self.request.sendall(response.encode())
+
+
+
+    def redirect_301(self,path):
+        date = self.get_time_formated()
+        http_status = "HTTP/1.1 301 Found"
+        mime_type = 'html'
+        content = "<HTML><HEAD>\r\n" + \
+                    "<TITLE>301 Moved</TITLE></HEAD><BODY>\r\n" + \
+                    "<H1>302 Moved</H1>\r\n" + \
+                    "The document has moved\r\n" + \
+                    "<A HREF=" + path + "/index.html>here</A>.\r\n" + \
+                    "</BODY></HTML>"
+        location = "Location: " + path + "/index.html"
+
+        response = http_status + "\r\n" + \
+                "Server: " + "Nick's Server" + "\r\n" + \
+                "Date: " + str(date) + "\r\n" + \
+                "Content-type: text/" + mime_type + "; charset=UTF-8" + "\r\n" \
+                "Content-length: " + str(len(content)) + "\r\n\r\n" + \
+                content + "\r\n"
+
+        self.request.sendall(response.encode())
+        self.request.sendall(location.encode())
+    
+
+
+    def not_found_404(self):
+        date = self.get_time_formated()
+        http_status = "HTTP/1.1 404 Not Found"
+        mime_type = "html"
+        contents = "<html><head>\r\n" + \
+                    "<title>404 Not Found</title>\r\n" + \
+                    "</head><body>\r\n" + \
+                    "<h2>Sorry, we cant find that page</h2>\r\n" + \
+                    "<h3>Error code: 404</h3>\r\n" + \
+                    "</body></html>"
+        response = http_status + "\r\n" + \
+                    "Server: " + "Nick's Server" + "\r\n" + \
+                    "Date: " + str(date) + "\r\n" + \
+                    "Content-type: text/" + mime_type + "; charset=UTF-8" + "\r\n" + \
+                    "Content-length: " + str(len(contents)) + "\r\n\r\n" + \
+                    contents + "\r\n"
+
+        self.request.sendall(response.encode())
+
+
+
+    def method_not_allowed_405(self):
+        date = self.get_time_formated()
+        http_status = "HTTP/1.1 405 Method Not Allowed"
+        mime_type = 'html'
+        contents = "<html><head>\r\n" + \
+                        "<title>405 Method Not Allowed</title>\r\n" + \
+                        "<h2>Sorry, that is not allowed.</h2>\r\n" + \
+                        "<h3>Error code: 405</h3>\r\n" + \
+                        "</body></html>"
+        response = http_status + "\r\n" + \
+                    "Server: " + "Nick's Server" + "\r\n" + \
+                    "Date: " + str(date) + "\r\n" + \
+                    "Content-type: text/" + mime_type + "; charset=UTF-8" + "\r\n" + \
+                    "Content-length: " + str(len(contents)) + "\r\n\r\n" + \
+                    contents + "\r\n"
+
+        self.request.sendall(response.encode())
+
+
+
+    def get_mime_type(self, uri):
+        
+        uri_type = uri.split(".")[-1]
+        mime_type = 'html'
+
+        if (uri_type == 'css'):
+            mime_type = 'css'
+        
+        return mime_type
+
+
+    def append_path(self, uri):
+        file = os.path.abspath('www'+uri)
+        return file
+
+    def get_time_formated(self):
+        now = datetime.now()
+        stamp = mktime(now.timetuple())
+        date = format_date_time(stamp)
+
+        return date
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +187,4 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
